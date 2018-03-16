bcce709b78ad9e49fefd137df8700b634afd5cf7
carlos <carlos.theran@upr.edu>
2018-03-16 05:01:56 -0400

    Initial Cheking of sale tool

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+	
+	public static void main(String[] arg) {
+		SalesData data= new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	
+	private static void displayGreeting() {
+		System.out.println("Hello Happy Sale People");
+		System.out.println("This app shows sales data");
+	}
+
+}
